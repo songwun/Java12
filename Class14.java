f36ea47f6c6f5accf196ab9d9b4ad11c02db314e
songwun <92703380+songwun@users.noreply.github.com>
2022-01-02 14:04:40 +0800

    Add files via upload

---
+++ Class14.java
@@ -0,0 +1,12 @@
+package Java12;
+
+public class Class14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Habit is second nature.";
+		System.out.println("string="+str);
+		System.out.println("sub-string="+str.substring(16,22));
+	}
+
+}
